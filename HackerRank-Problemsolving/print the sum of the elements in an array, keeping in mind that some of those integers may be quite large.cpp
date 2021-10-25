7105484193804afda89ac525fd15afd3cfb87bf5
★[ᴍᴀʏᴀɴᴋ ʙʜᴀʀᴅᴡᴀᴊ]★ <80567208+Mayank50082785@users.noreply.github.com>
2021-10-25 16:52:16 +0530

    Create  print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.cpp

---
+++ HackerRank-Problemsolving/print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+    cin>>num;
+    unsigned long long int sum=0,in;
+    for(int i=0;i<num;i++)
+    {
+    cin>>in;
+    sum=sum+in;
+    }
+    cout<<sum<<endl;
+}
