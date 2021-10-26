9167e9536925e68abe8a33f60f36041ab00a5542
★[ᴍᴀʏᴀɴᴋ ʙʜᴀʀᴅᴡᴀᴊ]★ <80567208+Mayank50082785@users.noreply.github.com>
2021-10-26 16:38:51 +0530

    Create 4.Birthday_Cake_Candles.cpp

---
+++ HackerRank-Problemsolving/4.Birthday_Cake_Candles.cpp
@@ -0,0 +1,16 @@
+You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+  
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    cout<<count(a,a+n,*max_element(a,a+n));
+    return 0 ;
+}
