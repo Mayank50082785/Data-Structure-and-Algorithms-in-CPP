b1bf73a61325850fc98f033a073e3bef8bf88529
★[ᴍᴀʏᴀɴᴋ ʙʜᴀʀᴅᴡᴀᴊ]★ <80567208+Mayank50082785@users.noreply.github.com>
2021-11-01 23:55:30 +0530

    Create Grading_students.cpp

---
+++ HackerRank-Problemsolving/Grading_students.cpp
@@ -0,0 +1,44 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+#ifndef Mayank
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    for(int a0 = 0; a0 < n; a0++){
+        int grade;
+        cin >> grade;
+        if (grade >= 38) {
+            int rem = grade % 5;
+            if (rem >= 3) grade += 5 - rem;
+        }
+        cout << grade << endl;
+    }
+    return 0;
+}
