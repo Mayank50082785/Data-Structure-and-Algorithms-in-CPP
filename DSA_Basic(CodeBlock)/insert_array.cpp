1e305ef23c377332f1de45eb1aaecd5733d0c769
★[ᴍᴀʏᴀɴᴋ ʙʜᴀʀᴅᴡᴀᴊ]★ <80567208+Mayank50082785@users.noreply.github.com>
2021-11-03 17:24:04 +0530

    Update insert_array.cpp

--- DSA_Basic(CodeBlock)/insert_array.cpp
+++ DSA_Basic(CodeBlock)/insert_array.cpp
@@ -14,7 +14,7 @@ struct Array
   int i;
   cout<<"\nElemnts are\n"<<endl;
   for(i=0;i<arr.length;i++)
-  cout<<arr.A[i];
+  cout<<arr.A[i]<<", ";
  }
 //Main Append Logic 
  void Append(struct Array *arr,int x)
@@ -22,10 +22,22 @@ struct Array
   if(arr->length<arr->size)
   arr->A[arr->length++]=x;
  }
+//Insert () Main Logic
+void Insert (struct Array *arr,int index,int x)
+{
+ int i;
+ if(index>=0 && index<=arr->length){
+  for(i=arr->length;i>index;i--)
+  arr->A[i]=arr->A[i-1];
+  arr->A[index]=x;
+  arr->length++;
+ }
+
+}
 //THE main() is used to execute all function struct(),Display(),Append()
   int main(){
   struct Array arr={{2,3,4,5,6},10,5};
-  Append(&arr,10);
+  Insert(&arr,2,10);
   Display(arr);
   return 0;
  }
